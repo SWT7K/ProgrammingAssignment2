853149e64b937f797e8aff4a0ed90b95677cd480
SWT7K <85085239+SWT7K@users.noreply.github.com>
2021-06-03 13:56:06 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Set of functions that save an inverse of a matrix and later checks
+# if the matrix already has been inverted (and hasn't changed)
 
-## Write a short comment describing this function
+# This function will set up the cache matrix parts so that the other 
+# function (cacheSolve) can check if it doesn't need to invert
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(matr = matrix()) {
+      matr2 <- NULL
+      set <- function(matr3) {
+          matr <<- matr3
+          matr2 <<- NULL
+      }
+      
+      get <- function() matr
+      setinverse <- function(inverse) matr2 <<- inverse
+      getinverse <- function() matr2
+      list(set = set,
+           get = get,
+           setinverse = setinverse,
+           getinverse = getinverse)
 }
+# This function will take the inverse, but only if needed.  It first
+# checks to see if the original matrix is the same, and if the inverse
+# has already been computed.
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(matr, ...) {
+      matr2 <- matr$getinverse()
+      
+      # does inverse already exist (original the same)
+      
+      if (!is.null(matr2)) {
+          message("getting cached data")
+          return(matr2) # 'return' ends function, saves compute time
+      }
+      
+      matr4 <- matr$get()
+      matr2 <- solve(matr4, ...)
+      matr$setinverse(matr2)
+      matr2
 }
